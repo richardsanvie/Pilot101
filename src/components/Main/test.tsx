b236c3ecb8da95eef44462325db4a74e9f3cd1f0
unknown <richardsanvie@gmail.com>
2024-10-20 11:10:53 -0300

    Configure test and git hook

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // renderiza o component
+    render(<Main />)
+
+    // busca o elemento e verifica a existência dele
+  })
+})
