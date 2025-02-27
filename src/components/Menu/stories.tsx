629c7cf2db10fbcc63358b5d84c89a73c7bbeefb
Matheus Richard Sanvie <Matheus.Sanvie@antaq.gov.br>
2025-02-27 14:06:57 -0300

    Add Footer Component

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,12 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Menu from '.'
+import Menu, {MenuProps} from '.'
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<MenuProps> = {
+  render: (args) => <Menu {...args} />
+}
 
 Default.parameters = { 
   layout: 'fullscreen',
