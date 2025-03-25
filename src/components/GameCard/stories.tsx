55a2ddee0dcfdf3348f2e03e7274ac02c615c000
Matheus Richard Sanvie <Matheus.Sanvie@antaq.gov.br>
2025-03-25 16:52:14 -0300

    Add GameCard 110 Component

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://picsum.photos/300/140',
+    price: 'R$235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj<GameCardProps> = {
+  render: (args) => (
+    <div style={{ width: '30rem' }}>
+      <GameCard {...args} />
+    </div>
+  )
+}
